a66a06f540fc9539c19fee9fe7e5404768939095
rollingsmajiwa <rollingsmajiwa@gmail.com>
2026-09-13 18:38:28 +0300

    Feat: create calorie reset button

--- calorie_tracker/urls.py
+++ calorie_tracker/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path("", views.calorie_list, name="home"),
     path("about/", views.aboutproject, name="about"),
     path("services/", views.servicesproject, name="services"),
-    path("contact/", views.contactproject, name="contact")
+    path("contact/", views.contactproject, name="contact"),
+    path("reset/", views.reset_calories, name="reset_calories"),
+    path("delete/<int:id>", views.delete_food, name="delete_food")
 ]
--- calorie_tracker/views.py
+++ calorie_tracker/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import FoodItem
 
 # Create your views here.
@@ -12,5 +12,28 @@ def servicesproject(request):
     context = {"message": "Wecome to my services section"}
     return render(request, "calorie_tracker/services.html", context)
 def contactproject(request):
-    context = {"message": "Wecome to my contact section"}
+    
+    if request.method == "POST":
+        title = request.POST.get("title")
+        calorie_amount = request.POST.get("calorie_amount")
+        if title and calorie_amount:
+            FoodItem.objects.create(title=title, calorie_amount=calorie_amount)
+            return redirect("home")
+    food_items = FoodItem.objects.all()
+    
+
+    total_calories = 0
+    for cal in food_items:
+        total_calories += cal.calorie_amount
+    context = {"message":"Calorie Tracker", "food_items": food_items, "total_calories": total_calories}
     return render(request, "calorie_tracker/contact.html", context)
+
+def delete_food(request, id):
+    if request.method == "POST":
+        item = FoodItem.objects.get(id=id)
+        item.delete()
+    return redirect("home")
+def reset_calories(request):
+    if request.method == "POST":
+        FoodItem.objects.all().delete()
+    return redirect("home")
